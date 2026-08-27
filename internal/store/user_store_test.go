585261c0a34b322c2e640cb6cdf24de18a9853a7
fs1g17 <fs1g17@soton.ac.uk>
2026-08-27 13:22:45 +0400

    feat: use a single docker service, add test_db database instead

--- internal/store/user_store_test.go
+++ internal/store/user_store_test.go
@@ -8,14 +8,14 @@ import (
 )
 
 func getTestStore(t *testing.T) *UserStore {
-	connString := "host=localhost user=postgres password=postgres dbname=postgres port=5433 sslmode=disable"
+	connString := "host=localhost user=postgres password=postgres dbname=test_db port=5432 sslmode=disable"
 
 	err := Migrate(connString)
 	if err != nil {
 		t.Fatalf("Failed to run test migrations: %v\n", err)
 	}
 
-	pgConn := Connect("postgres://postgres:postgres@localhost:5433/postgres")
+	pgConn := Connect("postgres://postgres:postgres@localhost:5432/test_db")
 	userStore := NewUserStore(pgConn)
 	return userStore
 }
@@ -33,4 +33,22 @@ func TestCreateUser(t *testing.T) {
 		t.Fatalf("expected 0 users but got %d\n", userCount)
 	}
 
+	user_id, err := userStore.CreateUser("theo", "password")
+	if err != nil {
+		t.Fatalf("didn't expect error, got: %v\n", err)
+	}
+
+	if user_id != 1 {
+		t.Fatalf("user_id, got: %d, want: %d\n", user_id, 1)
+	}
+
+	// actually check db has this user
+	var username string
+	err = userStore.conn.QueryRow(context.Background(), "SELECT username FROM users WHERE id = $1;", user_id).Scan(&username)
+	if err != nil {
+		t.Fatalf("didn't expect error, got: %v\n", err)
+	}
+	if username != "theo" {
+		t.Fatalf("got: %s, want: %s\n", username, "theo")
+	}
 }
