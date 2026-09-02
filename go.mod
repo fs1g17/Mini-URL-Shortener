10ac7c66dcf7231b6eb91b26ab10bf8ca99898d1
fs1g17 <fs1g17@soton.ac.uk>
2026-09-02 15:25:47 +0400

    test(link_store): add create shortened url test

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/fs1g17/Mini-URL-Shortener
 
-go 1.26
+go 1.26.0
 
 require (
 	github.com/golang-jwt/jwt/v5 v5.3.1
--- internal/store/link_store_test.go
+++ internal/store/link_store_test.go
@@ -6,166 +6,90 @@ import (
 	"time"
 )
 
-func TestCreateShortenedURL(t *testing.T) {
+func TestCreateShortenedUrl(t *testing.T) {
+	tests := []struct {
+		Name       string
+		Expire     *time.Time
+		ClickLimit *int
+	}{
+		{
+			Name: "should create shortened url with no expiration for existing user",
+		},
+		{
+			Name:       "should create shortened url with click expiration for existing user",
+			ClickLimit: new(10),
+		},
+		{
+			Name:   "should create shortened url with expiry for existing user",
+			Expire: new(time.Date(2009, 11, 17, 20, 34, 58, 651387237, time.UTC)),
+		},
+	}
 
-	t.Run("create shortened url with no expiration for existing user", func(t *testing.T) {
-		linkStore := getTestLinkStore(t)
-		userStore := getTestUserStore(t)
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			linkStore := getTestLinkStore(t)
+			userStore := getTestUserStore(t)
 
-		user_id, err := userStore.CreateUser("theo", "password")
-		if err != nil {
-			t.Fatalf("expected no error, got: %v\n", err)
-		}
+			user_id, err := userStore.CreateUser("theo", "password")
+			if err != nil {
+				t.Fatalf("expected no error, got: %v\n", err)
+			}
 
-		slug, err := linkStore.CreateShortenedURL("https://google.com", user_id, ShortUrlConfig{})
-		if err != nil {
-			t.Fatalf("expected no error, got: %v\n", err)
-		}
+			slug, err := linkStore.CreateShortenedURL("https://google.com", user_id, ShortUrlConfig{ClickLimit: tt.ClickLimit, Expire: tt.Expire})
+			if err != nil {
+				t.Fatalf("expected no error, got: %v\n", err)
+			}
 
-		var redirect_url string
-		var click_count int
-		var click_limit *int
-		var expires *string
-		var owner_id int
+			var redirect_url string
+			var click_count int
+			var click_limit *int
+			var expires *time.Time
+			var owner_id int
 
-		// verify slug is in the database
-		err = linkStore.conn.QueryRow(
-			context.Background(),
-			"SELECT redirect_url,click_count,click_limit,expires,owner_id FROM links WHERE slug = $1;",
-			slug,
-		).Scan(
-			&redirect_url, &click_count, &click_limit, &expires, &owner_id,
-		)
+			// verify slug is in the database
+			err = linkStore.conn.QueryRow(
+				context.Background(),
+				"SELECT redirect_url,click_count,click_limit,expires,owner_id FROM links WHERE slug = $1;",
+				slug,
+			).Scan(
+				&redirect_url, &click_count, &click_limit, &expires, &owner_id,
+			)
 
-		if err != nil {
-			t.Fatalf("want: nil, got: %v\n", err)
-		}
+			if err != nil {
+				t.Fatalf("want: nil, got: %v\n", err)
+			}
 
-		if redirect_url != "https://google.com" {
-			t.Fatalf("want: https://google.com, got: %s\n", redirect_url)
-		}
+			if redirect_url != "https://google.com" {
+				t.Fatalf("want: https://google.com, got: %s\n", redirect_url)
+			}
 
-		if click_count != 0 {
-			t.Fatalf("want: 0, got: %d\n", click_count)
-		}
+			if click_count != 0 {
+				t.Fatalf("want: 0, got: %d\n", click_count)
+			}
 
-		if click_limit != nil {
-			t.Fatalf("want: nil, got: %v\n", click_limit)
-		}
+			if click_limit == nil && tt.ClickLimit != nil || click_limit != nil && tt.ClickLimit == nil {
+				t.Fatalf("want: %v, got: %v\n", tt.ClickLimit, click_limit)
+			}
 
-		if expires != nil {
-			t.Fatalf("want: nil, got: %v\n", expires)
-		}
+			if click_limit != nil && tt.ClickLimit != nil && *click_limit != *tt.ClickLimit {
+				t.Fatalf("want: %v, got: %v\n", tt.ClickLimit, click_limit)
+			}
 
-		if owner_id != user_id {
-			t.Fatalf("want: %d, got: %d\n", user_id, owner_id)
-		}
-	})
+			if expires == nil && tt.Expire != nil {
+				t.Fatalf("want: %v, got: %v\n", tt.Expire, expires)
+			}
 
-	t.Run("create shortened url with click expiration for existing user", func(t *testing.T) {
-		linkStore := getTestLinkStore(t)
-		userStore := getTestUserStore(t)
+			if expires != nil && tt.Expire == nil {
+				t.Fatalf("want: %v, got: %v\n", tt.Expire, expires)
+			}
 
-		user_id, err := userStore.CreateUser("theo", "password")
-		if err != nil {
-			t.Fatalf("expected no error, got: %v\n", err)
-		}
+			if expires != nil && !expires.Truncate(time.Microsecond).Equal(tt.Expire.Truncate(time.Microsecond)) {
+				t.Fatalf("want: %v, got: %v\n", tt.Expire, expires)
+			}
 
-		var limit int = 10
-		slug, err := linkStore.CreateShortenedURL("https://google.com", user_id, ShortUrlConfig{ClickLimit: &limit})
-		if err != nil {
-			t.Fatalf("expected no error, got: %v\n", err)
-		}
-
-		var redirect_url string
-		var click_count int
-		var click_limit *int
-		var expires *string
-		var owner_id int
-
-		// verify slug is in the database
-		err = linkStore.conn.QueryRow(
-			context.Background(),
-			"SELECT redirect_url,click_count,click_limit,expires,owner_id FROM links WHERE slug = $1;",
-			slug,
-		).Scan(
-			&redirect_url, &click_count, &click_limit, &expires, &owner_id,
-		)
-
-		if err != nil {
-			t.Fatalf("want: nil, got: %v\n", err)
-		}
-
-		if redirect_url != "https://google.com" {
-			t.Fatalf("want: https://google.com, got: %s\n", redirect_url)
-		}
-
-		if click_count != 0 {
-			t.Fatalf("want: 0, got: %d\n", click_count)
-		}
-
-		if click_limit == nil || *click_limit != 10 {
-			t.Fatalf("want: 10, got: %v\n", click_limit)
-		}
-
-		if expires != nil {
-			t.Fatalf("want: nil, got: %v\n", expires)
-		}
-
-		if owner_id != user_id {
-			t.Fatalf("want: %d, got: %d\n", user_id, owner_id)
-		}
-	})
-
-	t.Run("create shortened url with expiry for existing user", func(t *testing.T) {
-		linkStore := getTestLinkStore(t)
-		userStore := getTestUserStore(t)
-
-		user_id, err := userStore.CreateUser("theo", "password")
-		if err != nil {
-			t.Fatalf("expected no error, got: %v\n", err)
-		}
-
-		someTime := time.Date(2009, 11, 17, 20, 34, 58, 651387237, time.UTC)
-		slug, err := linkStore.CreateShortenedURL("https://google.com", user_id, ShortUrlConfig{Expire: &someTime})
-		if err != nil {
-			t.Fatalf("expected no error, got: %v\n", err)
-		}
-
-		var redirect_url string
-		var click_count int
-		var click_limit *int
-		var expires *time.Time
-		var owner_id int
-
-		// verify slug is in the database
-		err = linkStore.conn.QueryRow(
-			context.Background(),
-			"SELECT redirect_url,click_count,click_limit,expires,owner_id FROM links WHERE slug = $1;",
-			slug,
-		).Scan(
-			&redirect_url, &click_count, &click_limit, &expires, &owner_id,
-		)
-
-		if err != nil {
-			t.Fatalf("want: nil, got: %v\n", err)
-		}
-
-		if redirect_url != "https://google.com" {
-			t.Fatalf("want: https://google.com, got: %s\n", redirect_url)
-		}
-
-		if click_count != 0 {
-			t.Fatalf("want: 0, got: %d\n", click_count)
-		}
-
-		if expires == nil || expires.Equal(someTime) {
-			t.Fatalf("want: %s, got: %s\n", someTime.String(), expires.String())
-		}
-
-		if owner_id != user_id {
-			t.Fatalf("want: %d, got: %d\n", user_id, owner_id)
-		}
-	})
-
+			if owner_id != user_id {
+				t.Fatalf("want: %d, got: %d\n", user_id, owner_id)
+			}
+		})
+	}
 }
