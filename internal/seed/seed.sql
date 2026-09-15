e9698d22bce64c16745567e2e1c4cb6a3b1b8349
fs1g17 <fs1g17@soton.ac.uk>
2026-09-15 16:06:02 +0400

    feat(seed): add links

--- internal/seed/seed.sql
+++ internal/seed/seed.sql
@@ -5,6 +5,10 @@ SELECT 'user_' || n,
        '$2a$14$lfwdV/JNa/O54I37RJx2iOqW6xrI0UONv49JLYFgmocbPTwv4GxH.'
 FROM generate_series(0, 9) AS series(n);
 
--- INSERT INTO links (slug , redirect_url , click_count , created_at , updated_at , click_limit , expires , owner_id)
--- VALUES 
---   ()
+INSERT INTO links (slug , redirect_url , click_limit , expires , owner_id)
+SELECT lpad(n::text, 6, '0'),
+       CONCAT('https://example', n, '.com'),
+       NULL,
+       NULL,
+       DIV(n, 10) + 1
+FROM generate_series(0, 99) AS series(n);
